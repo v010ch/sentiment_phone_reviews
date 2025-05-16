2621f80dea94ddafdbf96566ace06eb9a444a16f
v010ch <iakhremenko.v@gmail.com>
2025-05-16 17:21:05 +0300

    flask app rework
    +now redirect on review page instead render review page from same method
    +parameters from html are now received from request.form
    +added logic to work as client. server on docker

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,99 +1,110 @@
 ''' flask main app for phone review sentiment analysis'''
-import os
-from pathlib import Path
+import socket
 from typing import Optional
-from flask import Flask, render_template, request
-from sentiment_analyze_models import ModelTfIdf, ModelROBERTA
+
+from flask import Flask, render_template, request, redirect, url_for
+
+
+HOST = '172.17.0.2'
+PORT = 9126
+
 
 app = Flask(__name__)
 
-
-setup = {'tfidf': True, 'roberta': 'base'}
-
-print('init')
-model_selected = False
-model = None
-
-# проверка можем ли использовать лемматизатор и стеммер для легковесной модели
-try:
-    import nltk.stem as st
-    import nltk.data
-    nltk.data.find('stemmers/rslp')
-    setup['stem'] = True
-except ImportError:
-    print('lemm/stem fail')
-    setup['stem'] = False
+socket_cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_cli.connect((HOST, PORT))
 
 
-# проверка можем ли использовать среднюю и тяжелую модель
-try:
-    import torch
-    from transformers import pipeline
-    setup['roberta'] = True
-except ImportError:
-    print('transformers fail')
-    setup['roberta'] = False
+def send_data(inp_msg: str):
+    '''
+    '''
+    socket_cli.send(inp_msg.encode('utf-8'))
 
 
-# print(os.path.join(Path.cwd().parents[0], 'models'))
-# MODELS_PATH = os.path.join(Path.cwd().parents[0], 'models')
+def get_data() -> str:
+    '''
+    '''
+    data = socket_cli.recv(1024)
+    if not data:
+        raise 'Out of data from server'
 
-
-def get_model(inp_type: str):
-    global setup
-
-    if inp_type == 'tf-idf':
-        return ModelTfIdf(setup, 'tfidf_lr')
-    elif inp_type == 'base':
-        setup['roberta'] = 'base'
-        return ModelROBERTA(setup, 'cardiffnlp/twitter-roberta-base-sentiment')
-    else:  # inp_type == 'large'
-        setup['roberta'] = 'large'
-        return ModelROBERTA(setup, 'siebert/sentiment-roberta-large-english')
-
+    return data.decode('utf-8')
 
 
 @app.route("/", methods=["POST", "GET"])
-def index_page(text: Optional[str] = '',
-               model_type: Optional[str] = '',
-               prediction_message: Optional[str] = '',
-               ):
-    global model_selected
-    global model
+def index_page():
+    '''
+    '''
+    # rudiment
+    # chech which model can we use
+    print(f'index_page: {request.method}')
+    if request.method == 'GET':
+        command = 'get_available_models'
+        socket_cli.send(command.encode('utf-8'))
 
+        available_models = get_data()
+        print(available_models)
+
+    # tell to server to load the selected model
     if request.method == "POST":
-        if model_selected: 
-            text = request.form["text"]
-        else:
-            model_type = request.form["model_type"]
-            model_selected = True
-            print(model_type)
-            model = get_model(model_type)
-            print('model loaded')
+        print(request.form['model_type'])
+        command = 'load_model'
+        socket_cli.send(command.encode('utf-8'))
 
-    # различные страницы для выбора типа модели и проверки отзыва
-    if model_selected:
-        if text != '':
-            prediction_message = model.predict_tonality(text)
-        else:
-            prediction_message = 'neutral'
-        return render_template('input_review.html', text=text,
-                               model_type=model_type,
-                               prediction_message=prediction_message,
-                               )
+        model = request.form['model_type']
+        socket_cli.send(model.encode('utf-8'))
+
+        return redirect(url_for('review_page'))
 
     # no model choosed. check can we use medium and large models
-    if setup['roberta']:
-        return render_template('index.html', text=text,
-                               model_type=model_type,
-                               prediction_message=prediction_message,
-                               )
+    if available_models == 'all':
+        return render_template('index.html',)
     else:
-        return render_template('index_noroberta.html', text=text,
-                               model_type=model_type,
-                               prediction_message=prediction_message,
+        return render_template('index_noroberta.html',)
+
+
+@app.route("/review", methods=["POST", "GET"])
+def review_page(text: Optional[str] = '', 
+                prediction_message: Optional[str] = ''
+                ):
+    '''
+    '''
+    print(f'review_page: {request.method}')
+    if request.method == "GET":
+        if 'model type' in request.form.keys():
+            print(request.form['model_type'])
+
+        return render_template('input_review.html',  # text=text,
+                               # model_type=model_type,
+                               # prediction_message=prediction_message,
+                               )
+
+    if request.method == "POST":
+        if 'text' in request.form.keys():
+            review = request.form['text']
+            print(review)
+
+            command = 'get_sentiment'
+            socket_cli.send(command.encode('utf-8'))
+
+            # often two send in a row work like one send
+            # should devide them with recv
+            # getting unusefull data
+            _ = get_data()
+
+            model = review
+            socket_cli.send(model.encode('utf-8'))
+
+            sentiment = get_data()
+        else:
+            print(request.form)
+
+        return render_template('input_review.html', text=review,
+                               # model_type=model_type,
+                               prediction_message=sentiment,
                                )
 
 
 if __name__ == "__main__":
+    # app.run(port=int(os.getenv('PORT', 31031)))
     app.run()
